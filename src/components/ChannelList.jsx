e0728664bda4251eaeea5e11e46bdfc8e228f88f
ushachev <vit.ush@mail.ru>
2021-11-17 20:35:16 +0300

    temporary remove Simplebar

--- src/components/ChannelList.jsx
+++ src/components/ChannelList.jsx
@@ -1,13 +1,13 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Button } from 'react-bootstrap';
-import SimpleBar from 'simplebar-react';
+// import SimpleBar from 'simplebar-react';
 import { IoIosAddCircleOutline } from 'react-icons/io';
 import { useTranslation } from 'react-i18next';
 
 import { selectChannelList } from '../store/reducers/channelsSlice.js';
 import {
-  selectCurrentChannelId, selectDefaultChannelId,
+  selectCurrentChannelId,
 } from '../store/reducers/activeChannelSlice.js';
 import { setModal } from '../store/reducers/modalSlice.js';
 
@@ -15,25 +15,25 @@ import Channel from './Channel.jsx';
 
 const ChannelList = function ChannelList() {
   const dispatch = useDispatch();
-  const scrollableNodeRef = useRef();
+  // const scrollableNodeRef = useRef();
   const channels = useSelector(selectChannelList);
   const currentChannelId = useSelector(selectCurrentChannelId);
-  const defaultChannelId = useSelector(selectDefaultChannelId);
+  // const defaultChannelId = useSelector(selectDefaultChannelId);
   const { t } = useTranslation();
 
-  useEffect(() => {
-    const { current: channelListEl } = scrollableNodeRef;
-    const lastChannel = channels[channels.length - 1];
+  // useEffect(() => {
+  //   const { current: channelListEl } = scrollableNodeRef;
+  //   const lastChannel = channels[channels.length - 1];
 
-    if (currentChannelId === defaultChannelId) {
-      channelListEl.scrollTop = 0;
-      return;
-    }
+  //   if (currentChannelId === defaultChannelId) {
+  //     channelListEl.scrollTop = 0;
+  //     return;
+  //   }
 
-    if (currentChannelId === lastChannel?.id) {
-      channelListEl.scrollTop = channelListEl.scrollHeight;
-    }
-  }, [channels]);
+  //   if (currentChannelId === lastChannel?.id) {
+  //     channelListEl.scrollTop = channelListEl.scrollHeight;
+  //   }
+  // }, [channels]);
 
   const handleAddChannel = () => {
     dispatch(setModal({ type: 'adding' }));
@@ -55,16 +55,14 @@ const ChannelList = function ChannelList() {
         </Button>
       </div>
       <div className="mt-1 overflow-hidden">
-        <SimpleBar scrollableNodeProps={{ ref: scrollableNodeRef }} className="mh-100">
-          <ul className="list-unstyled">
-            {channels.map((channel) => {
-              const { id } = channel;
-              return (
-                <Channel key={id} channel={channel} current={id === currentChannelId} />
-              );
-            })}
-          </ul>
-        </SimpleBar>
+        <ul className="list-unstyled">
+          {channels.map((channel) => {
+            const { id } = channel;
+            return (
+              <Channel key={id} channel={channel} current={id === currentChannelId} />
+            );
+          })}
+        </ul>
       </div>
     </>
   );
--- src/components/MessageBox.jsx
+++ src/components/MessageBox.jsx
@@ -1,30 +1,28 @@
-import React, { useRef, useEffect } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
-import SimpleBar from 'simplebar-react';
+// import SimpleBar from 'simplebar-react';
 
 import { selectCurrentMessageList } from '../store/reducers/messagesSlice.js';
 
 const MessageBox = function MessageBox() {
-  const scrollableNodeRef = useRef();
+  // const scrollableNodeRef = useRef();
   const messageList = useSelector(selectCurrentMessageList);
 
-  useEffect(() => {
-    scrollableNodeRef.current.scrollTop += (scrollableNodeRef.current.scrollHeight
-      - scrollableNodeRef.current.clientHeight);
-  }, [messageList]);
+  // useEffect(() => {
+  //   scrollableNodeRef.current.scrollTop += (scrollableNodeRef.current.scrollHeight
+  //     - scrollableNodeRef.current.clientHeight);
+  // }, [messageList]);
 
   return (
     <div className="mt-auto overflow-hidden">
-      <SimpleBar scrollableNodeProps={{ ref: scrollableNodeRef }} className="mh-100">
-        <ul className="list-unstyled">
-          {messageList.map(({ id, username, body }) => (
-            <li key={id} className="message py-1 ps-5 pe-4 text-break">
-              <span className="me-2 fw-bold text-body">{`${username}:`}</span>
-              <span>{body}</span>
-            </li>
-          ))}
-        </ul>
-      </SimpleBar>
+      <ul className="list-unstyled">
+        {messageList.map(({ id, username, body }) => (
+          <li key={id} className="message py-1 ps-5 pe-4 text-break">
+            <span className="me-2 fw-bold text-body">{`${username}:`}</span>
+            <span>{body}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
